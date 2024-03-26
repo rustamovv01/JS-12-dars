bb23b8c44ca019e19c78cc17f8f24670dc6b3a5d
Mukhammadaziz <rustamov977075073@gmail.com>
2024-03-26 14:10:53 +0500

    12-dars

---
+++ main.js
@@ -0,0 +1,28 @@
+let son = +prompt("son kiriting: ");
+
+switch (son) {
+    case 1:
+        alert("Dushanba");
+        break;
+    case 2:
+        alert("Seshanba");
+        break;
+    case 3: 
+        alert("Chorshanba");
+        break;
+    case 4: 
+        alert("Payshanba");
+        break;
+    case 5:
+        alert("Juma");
+        break;
+    case 6:
+        alert("Shanba");
+        break;
+    case 7: 
+        alert("Yakshanba");
+        break;
+    default: 
+        alert("ma'lumot topilmadi");
+        break;
+}
